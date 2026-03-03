e2be9e525e86e0039b6cac0fa31e192cd02c3e8d
g00099417-hub <g00099417@aus.edu>
2026-03-03 04:39:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("messageBtn").addEventListener("click", function() {
+
+    document.getElementById("output").innerHTML =
+        "Thank you for visiting my portfolio. I look forward to connecting with you!";
+
+});
